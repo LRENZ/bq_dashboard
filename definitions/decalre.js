6e136d5e798a625af1e33a566284d4cd32cc5bae
Gary L <renzhong.l@fivestones.net>
2025-11-28 06:23:06 +0000

    first

---
+++ definitions/decalre.js
@@ -0,0 +1,31 @@
+declare({
+  database: 'bigquery-2024',
+  schema: 'analytics_489554557',
+  name: 'events_*',
+});
+
+declare({
+  database: 'bigquery-2024',
+  schema: 'analytics_489554557',
+  name: 'events_20251111',
+});
+
+declare({
+  database: 'bigquery-2024',
+  schema: 'attribution_v1',
+  name: 'mta_attribution_v1_markov_details',
+});
+
+
+/*
+declare({
+  database: 'bigquery-2024',
+  schema: 'attribution',
+  name: 'mta_attribution_test_markov_details',
+});
+
+declare({
+  database: 'bigquery-2024',
+  schema: 'attribution',
+  name: 'channel_performance',
+});*/
